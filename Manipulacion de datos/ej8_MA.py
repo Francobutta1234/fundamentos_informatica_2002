1706428b72303dfe4ad0714b46d767174dec97f4
Francobutta1234 <90804208+Francobutta1234@users.noreply.github.com>
2022-04-18 09:06:52 -0300

    Delete Manipulacion de datos directory

--- Manipulacion de datos/ej8_MA.py
+++
@@ -1,8 +0,0 @@
-def pollo_vignolo(archivo1, archivo2, archivo3):
-    with open(archivo1, "r") as f, open(archivo2, "r") as a, open(archivo3, "a") as af:
-        for palabra in f.read():
-              af.write(palabra)
-        for letter in a.read():
-              af.write(letter)
-
-print(pollo_vignolo("bio.txt", "riber.txt", "tuteneta.txt"))
