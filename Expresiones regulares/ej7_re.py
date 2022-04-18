bfb25ee55225953d6d859bc5ac9a96c346948708
franco <francobuttazzoni1@gmail.com>
2022-04-18 20:03:04 -0300

    subi mas ejercicios

--- Expresiones regulares/ej7_re.py
+++ Expresiones regulares/ej7_re.py
@@ -1,1 +1,5 @@
 import re
+#7
+def ej7(string):
+    return re.findall("[A-Za-z0-9]", string)
+print(ej7("holaKJGKHGJH"))
--- Expresiones regulares/ej9_re.py
+++ Expresiones regulares/ej9_re.py
@@ -1,1 +1,5 @@
 import re
+#9
+def guiones(string):
+    return re.findall("-(.*?)-", string)
+print(guiones("fff -esto- ff  ggrgr-esto tambien-gg"))
