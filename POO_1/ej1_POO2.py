041116ecc5cbc435049fdeb37d58bfbdcf9449d6
franco <francobuttazzoni1@gmail.com>
2022-04-18 10:08:57 -0300

    Update ej1_POO2.py
    hice cambios

--- POO_1/ej1_POO2.py
+++ POO_1/ej1_POO2.py
@@ -1,3 +1,4 @@
 #sus estados son el alimento y las caricias
 #sus interfaces son: energia, comer, acariciar, estaDebil (estas las comparten los dos). Y el perro tambien cuenta con pasear y alimento. Y el gato cuenta con caricias, y el perro no.
 #hay polimorfismo parcial, ya que no comparten todas sus interfaces
+#
