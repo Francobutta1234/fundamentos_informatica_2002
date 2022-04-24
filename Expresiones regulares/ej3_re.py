2c7226702db8512ebed33ef82f991d1f78991407
franco <francobuttazzoni1@gmail.com>
2022-04-24 18:13:45 -0300

    cambios en regular expresions

--- Expresiones regulares/ej3_re.py
+++ Expresiones regulares/ej3_re.py
@@ -1,5 +1,10 @@
 import re
 def con_h(string):
-    return bool(re.search("he", string))
+    return bool(re.search("h?", string))
 
-print(con_h("homofobia"))  
+def he(string):
+    return bool(re.search("he+", string))
+
+def hee(string):
+    return bool(re.search("he{2, 4}", string))
+ 
