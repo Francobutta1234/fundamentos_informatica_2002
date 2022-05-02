752800072d02154f5d31f0d9b6cbde4a963a5489
franco <francobuttazzoni1@gmail.com>
2022-05-02 18:05:33 -0300

    re

--- Expresiones regulares/ej4_re.py
+++ Expresiones regulares/ej4_re.py
@@ -1,6 +1,5 @@
 import re
-def unidas(string, palabra1, palabra2):
-    return bool(re.search(palabra1 + "_" + palabra2, string))
-
-print(unidas("me llamo_franco", "me", "llamo"))
-print(unidas("me_llamo franco", "me", "llamo"))
+def unidas(string):
+    return re.findall("\D\w*_\D\w*", string)
+print(unidas("me llamo_franco"))
+print(unidas("me_llamo franco"))
